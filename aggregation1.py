4744482691ca790492a023a31626bcb4d471a338
Daniel Quintero <dgquintero02@hotmail.com>
2023-09-06 15:49:42 -0500

    aggregation update

--- aggregation1.py
+++ aggregation1.py
@@ -7,27 +7,40 @@ from bson.objectid import ObjectId
 
 MONGODB_URI = os.getenv("MONGODB_URI")
 
+# Connect to MongoDB cluster with MongoClient
 client = MongoClient(MONGODB_URI)
 
+# Get reference to 'bank' database
 db = client.bank
 
+# Get reference to 'accounts' collection
 accounts_collection = db.accounts
 
-select_by_balance = {"$match": {"balance": {"$lt": 100000000000000}}}
+# Calculate the average balance of checking and savings accounts with balances of less than $1000.
 
-separate_by_account_calculate_avg_valance = {
-    "$group": { "_id": "$account_type", "average_balance": {"$avg": "$balance"}}
+# Select accounts with balances of less than $1000.
+select_by_balance = {"$match": {"balance": {"$lt": 1000}}}
+
+# Separate documents by account type and calculate the average balance for each account type.
+separate_by_account_calculate_avg_balance = {
+    "$group": {"_id": "$account_type", "avg_balance": {"$avg": "$balance"}}
 }
 
+# Create an aggegation pipeline using 'stage_match_balance' and 'stage_group_account_type'.
+pipeline = [
+    select_by_balance,
+    separate_by_account_calculate_avg_balance,
+]
 
-pipeline = [select_by_balance, separate_by_account_calculate_avg_valance]
-
-result = accounts_collection.aggregate(pipeline)
+# Perform an aggregation on 'pipeline'.
+results = accounts_collection.aggregate(pipeline)
 
 print()
-print("Average balance per account type:")
+print(
+    "Average balance of checking and savings accounts with balances of less than $1000:", "\n"
+)
 
-for doc in result:
-    pprint.pprint(doc)
+for item in results:
+    pprint.pprint(item)
 
 client.close()
