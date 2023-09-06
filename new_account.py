fb00a75ba7836e6977a1fcfb0f534bc0945f1ed0
Daniel Quintero <dgquintero02@hotmail.com>
2023-09-06 14:51:23 -0500

    project

---
+++ new_account.py
@@ -0,0 +1,7 @@
+new_account = {
+  "account_holder": "Daniel Quintero",
+  "account_id": "123",
+  "account_type": "checking",
+  "balance": 12000000,
+  "last_update": datetime.datetime.utcnow()
+}
---
+++ sample_doc.py
@@ -0,0 +1,18 @@
+
+new_document = {
+    "account_holder": "Daniel",
+    "account_id": "123",
+    "account_type": "checking",
+    "years_active": 5,
+    "address": {
+        "street": "123 Main St",
+        "city": "San Francisco",
+        "state": "CA",
+        "zip": "94101"
+    },
+    "transactions": [
+      "TR6324",
+      "TR6325",
+      "TR6326"
+    ],
+}
